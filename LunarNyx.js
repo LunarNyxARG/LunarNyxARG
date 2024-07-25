ee5d107176a776c6275baee412c017a64b88af57
LunarNyxARG <lunarnyxarg@gmail.com>
2024-07-26 06:36:54 +0800

    Add files via upload

---
+++ LunarNyx.js
@@ -0,0 +1,5 @@
+function LunarNyx() {
+    if (document.getElementById("textbox").value == "bWFud2l0aHRoZWNyb3du") {
+        document.getElementById("main").style.backgroundImage = "url(./final.png)";
+    }
+}
